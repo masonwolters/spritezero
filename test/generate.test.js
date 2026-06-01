7b713d878f98c3e62d852d5622db65299d7f4fc0
Mason Wolters <masonw919@gmail.com>
2026-05-31 14:39:30 -1000

    Upgrade mapnik

--- test/generate.test.js
+++ test/generate.test.js
@@ -5,7 +5,7 @@ var test = require('tap').test,
     queue = require('queue-async'),
     stringify = require('json-stable-stringify'),
     spritezero = require('../'),
-    mapnik = require('mapnik');
+    mapnik = require('@mapnik/mapnik');
 
 // eslint-disable-next-line no-process-env
 var update = process.env.UPDATE;
